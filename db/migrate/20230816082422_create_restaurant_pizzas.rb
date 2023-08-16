5192cf41b487a8ef9f27e278ae5ee5c9bac6da8f
samuel <samuelkelvinmwangi@gmail.com>
2023-08-16 11:28:32 +0300

    making correctiond=s in the create pizza .rb

--- db/migrate/20230816082422_create_restaurant_pizzas.rb
+++ db/migrate/20230816082422_create_restaurant_pizzas.rb
@@ -2,8 +2,8 @@ class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurant_pizzas do |t|
       t.decimal :price
-      t.references :pizza, null: false, foreign_key: true
-      t.references :restaurant, null: false, foreign_key: true
+      t.references :pizza, foreign_key: true
+      t.references :restaurant, foreign_key: true
 
       t.timestamps
     end
