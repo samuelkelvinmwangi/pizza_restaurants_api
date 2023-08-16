c599e2ac2c431ef0ac883f755027e872f2309073
samuel <samuelkelvinmwangi@gmail.com>
2023-08-16 12:49:20 +0300

    adding contolls to restaturants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,25 @@
 class RestaurantsController < ApplicationController
+    def index
+        restaurants = Restaurant.all
+        render json: restaurants
+    end
+
+    def show
+        restaurant = Restaurant.find_by(id: params[:id])
+        if restaurant
+          render json: restaurant.as_json(include: { pizzas: { only: [:id, :name, :ingredients] } })
+        else
+          render json: { error: 'Restaurant not found' }, status: :not_found
+        end
+    end
+
+    def destroy
+        restaurant = Restaurant.find_by(id: params[:id])
+        if restaurant
+          restaurant.destroy
+          render json: {}, status: :no_content
+        else
+          render json: { error: 'Restaurant not found' }, status: :not_found
+        end
+    end
 end
