57eb9cd6614c5cccb59049016c025d9a55d150fe
samuel <samuelkelvinmwangi@gmail.com>
2023-08-16 14:08:12 +0300

    creating seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+# creating restaurants
+restaurant1 = Restaurant.create(name: "Dominos", address: "Kahawa 24")
+restaurant2 = Restaurant.create(name: "PizzaInn", address: "Githurai 44")
+
+# Create pizzas
+pizza1 = Pizza.create(name: 'Cheese', ingredients: 'Dough, Tomato Sauce, Cheese')
+pizza2 = Pizza.create(name: 'Pepperoni', ingredients: 'Dough, Tomato Sauce, Cheese, Pepperoni')
+
+# Create restaurant pizzas
+RestaurantPizza.create(price: 12, pizza: pizza1, restaurant: restaurant1)
+RestaurantPizza.create(price: 15, pizza: pizza2, restaurant: restaurant1)
+RestaurantPizza.create(price: 10, pizza: pizza1, restaurant: restaurant2)
