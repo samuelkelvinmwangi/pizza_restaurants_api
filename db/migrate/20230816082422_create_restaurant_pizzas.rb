27456903f25f6bf86817219d277b5bbb4b24a705
samuel <samuelkelvinmwangi@gmail.com>
2023-08-16 11:24:57 +0300

    generating model for pizza prices

---
+++ db/migrate/20230816082422_create_restaurant_pizzas.rb
@@ -0,0 +1,11 @@
+class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :restaurant_pizzas do |t|
+      t.decimal :price
+      t.references :pizza, null: false, foreign_key: true
+      t.references :restaurant, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
