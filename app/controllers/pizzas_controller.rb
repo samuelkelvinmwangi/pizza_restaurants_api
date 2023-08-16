9f3b051117a25a3766db259c460906f03be1a159
samuel <samuelkelvinmwangi@gmail.com>
2023-08-16 22:20:51 +0300

    adding changes to the file to remove created time line

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,6 +1,6 @@
 class PizzasController < ApplicationController
     def index
         pizzas = Pizza.all
-        render json: pizzas
+        render json: pizzas.as_json(only: [:id, :name, :ingredients])
     end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,13 @@
 class RestaurantsController < ApplicationController
     def index
         restaurants = Restaurant.all
-        render json: restaurants
+        render json: restaurants.as_json(only: [:id, :name, :address])
     end
 
     def show
         restaurant = Restaurant.find_by(id: params[:id])
         if restaurant
-          render json: restaurant.as_json(include: { pizzas: { only: [:id, :name, :ingredients] } })
+          render json: restaurant.as_json( only: [:id, :name, :address], include: { pizzas: { only: [:id, :name, :ingredients] } })
         else
           render json: { error: 'Restaurant not found' }, status: :not_found
         end
